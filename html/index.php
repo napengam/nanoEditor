0e11f7bebd911c60adb11dabd677acc678542248
Heinz <Heinz@Heinz-PC.fritz.box>
2020-06-28 16:49:34 +0200

    remove contextmenu when saving html

--- html/index.php
+++ html/index.php
@@ -9,20 +9,20 @@
         <script>
 
             function initEditDemo() {
-                var objects, nanoEdit = createEditor({'imageUploadPath': ''});
-                objects = document.body.querySelectorAll('.editMe'); 
+                var objects, nanoEdit = createEditor({'imageUploadPath': '../upload/'});
+                objects = document.body.querySelectorAll('.editMe');
                 [].forEach.call(objects, function (obj) {
                     obj.ondblclick = nanoEdit.attacheEditor;
                 });
             }
- 
+
             window.addEventListener('load', initEditDemo, false);
         </script>
     </head>
     <body>
 
         <div id="divdiv" class='editMe' style="display:inline-block">
-           
+
             <h1>double click here to start the editor</h1>
 
             <H2> Image upload is not <i>configured</i> in this demo </h2> 
@@ -35,12 +35,14 @@
                 Right click into a table cell and you get a context menu to add or remove rows and columns.
                 Inserting columns will work only in cells of type &lt;TD> with a colspan of maximum 1.
             </p>
-
+          
             <table>
                 <tr><th>th1</th><th colspan="2">th2</th><th>th3</th></tr>
                 <tr><td>eins</td><td> 2a </td><td> 2b </td><td> III </td></tr>
             </table>
-
+            <br>
+                
+            
         </div>
 
     </body>
