3c3f1546c83a2aefb27add4a387e8e7b11a1e6ee
Heinz <Heinz@Heinz-PC.fritz.box>
2021-08-11 12:48:15 +0200

    default value for an option and no value given is "1"

--- html/index.php
+++ html/index.php
@@ -3,21 +3,7 @@
     <head>
         <title>nanoEditor</title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <script src="https://use.fontawesome.com/ed46cb3bd2.js"></script>
 
-        <script src="../js/nanoEditor.js"></script>
-        <script>
-
-            function initEditDemo() {
-                var objects, nanoEdit = createEditor({'imageUploadPath': '../upload/'});
-                objects = document.body.querySelectorAll('.editMe');
-                [].forEach.call(objects, function (obj) {
-                    obj.ondblclick = nanoEdit.attacheEditor;
-                });
-            }
-
-            window.addEventListener('load', initEditDemo, false);
-        </script>
     </head>
     <body>
 
@@ -35,16 +21,29 @@
                 Right click into a table cell and you get a context menu to add or remove rows and columns.
                 Inserting columns will work only in cells of type &lt;TD> with a colspan of maximum 1.
             </p>
-          
+
             <table>
                 <tr><th>th1</th><th colspan="2">th2</th><th>th3</th></tr>
                 <tr><td>eins</td><td> 2a </td><td> 2b </td><td> III </td></tr>
             </table>
             <br>
-                
-            
-        </div>
 
+
+        </div>
+        <script src="https://use.fontawesome.com/ed46cb3bd2.js"></script>
+        <script src="../js/nanoEditor.js"></script>
+        <script>
+
+            function initEditDemo() {
+                var objects, nanoEdit = createEditor({'imageUploadPath': '../upload/'});
+                objects = document.body.querySelectorAll('.editMe');
+                [].forEach.call(objects, function (obj) {
+                    obj.ondblclick = nanoEdit.attacheEditor;
+                });
+            }
+
+            window.addEventListener('load', initEditDemo, false);
+        </script>
     </body>
 </html>
 
